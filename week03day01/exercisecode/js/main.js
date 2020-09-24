49e7d8fb8e09de65d3621f69a41ccb5e119b0501
Arr Belrey Domingo <59627816+snowman07@users.noreply.github.com>
2020-09-24 08:48:14 -0600

    done coding exercise for EVENTS

--- week03day01/exercisecode/js/main.js
+++ week03day01/exercisecode/js/main.js
@@ -1,13 +1,14 @@
-let featureLink = document.querySelector(".feature link");
+let featureLink = document.querySelector("a.link"); // OR (".feature.link");
 
-function featureLinkHandler() {
+const featureLinkHandler = function FunctionName(param) {
+  // featureLink.innerHTML =
+  //   "You clicked the link but you cant go to other page to show the image because I prevented the default";
+  // FunctionName.preventDefault();
   let featureImage = document.querySelector("img.feature");
-  featureImage.src = featureLink.href;
-  featureImage.classList.remove("hidden");
-}
+  featureImage.src = featureLink.href; //dont understand how this works
+  featureImage.classList.remove("hidden"); //dont understand how this works
 
-featureLink.addEventListener("click", function (featureLinkHandler) {
-  let featureImage = document.querySelector("img.feature");
-  featureImage.src = featureLink.href;
-  featureImage.classList.remove("hidden");
-});
+  param.preventDefault();
+};
+
+featureLink.addEventListener("click", featureLinkHandler);
