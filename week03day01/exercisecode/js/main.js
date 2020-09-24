1c9b170bccd1242d71b2afa588388422bd8435a5
Arr Belrey Domingo <59627816+snowman07@users.noreply.github.com>
2020-09-24 17:47:43 -0600

    just some comment on js file

--- week03day01/exercisecode/js/main.js
+++ week03day01/exercisecode/js/main.js
@@ -1,3 +1,5 @@
+// Description: User will click the link and image will show on the same page. Usually, when user click an anchor tag, link will open to other webpage. preventDefault() function is use to stop this from happening.
+
 let featureLink = document.querySelector("a.link"); // OR (".feature.link");
 
 const featureLinkHandler = function FunctionName(param) {
